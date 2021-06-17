39cbfa0291a9601609c3663eb3f948d2beb39545
Venkataraman TK <ramkalahasthy@gmail.com>
2021-06-17 07:35:47 +0530

    Update config.ts

--- config.ts
+++ config.ts
@@ -2,7 +2,7 @@ export const config = {
   invite: 'NTBlMGQzMDAtY2E4ZS0xMWViLWJkNzEtZjFmMjgyZGY3NjVkOmYwYTE0NjdhLTMyYzEtMTFlYi1iMWI4LTViYTQzMmY1ZjBkMA==',
   password: 'SvnKg7zFwkLc9mh9PAAWt',
   screenshotsDir: './screenshots',
-  runs: 80,
+  runs: 4,
 };
 
 export default config;
